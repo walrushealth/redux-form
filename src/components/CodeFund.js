78c328a9ff62fdbd5194f2968112c2aa71b9d47e
Erik Rasmussen <rasmussenerik@gmail.com>
2018-04-02 19:18:02 +0200

    Fixed codefund bug

--- src/components/CodeFund.js
+++ src/components/CodeFund.js
@@ -1,13 +1,31 @@
 import React from 'react'
 const id = '4f9aabe5-1dda-413e-b01e-3174c48e4762'
-const CodeFund = ({ template }) => (
-  <div>
-    <div id="codefund_ad" className="benevolent-sponsor" />
-    <script
-      src={`https://codefund.io/scripts/${id}/embed.js?template=${template ||
-        'centered'}&theme=dark`}
-    />
-  </div>
-)
 
-export default CodeFund
+export default class CodeFund extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      embedScript: false
+    }
+  }
+
+  componentDidMount() {
+    // this allows SSR component to be overridden and not lose its DOM manipulation
+    this.setState({ embedScript: true })
+  }
+
+  render() {
+    const { template } = this.props
+    return (
+      <div>
+        <div id="codefund_ad" className="benevolent-sponsor" />
+        {this.state.embedScript && (
+          <script
+            src={`https://codefund.io/scripts/${id}/embed.js?template=${template ||
+              'centered'}&theme=dark`}
+          />
+        )}
+      </div>
+    )
+  }
+}
